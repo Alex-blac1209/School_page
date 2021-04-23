a9027594ca7d5b6e638b31c0e548d40a9b54cde3
Alex <aleksmaliszews@gmail.com>
2021-04-23 13:51:35 +0200

    I need help please!!! :( I create a sidebar menu with submenu, buuuut: - navbar covers the home page a little bit; - transition doesn't work; - If you don't like it - just delete it; - we should think about responsiveness :b; - I added some icons (they look cool B) ); I try to make a wave animation, but I fail (sorry :(( ); (Sorry for my linguistic mistakes ^-^)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,62 @@
-let menu      = document.querySelector("nav");
-let menuClose = document.querySelector("nav .close");
-let menuOpen  = document.querySelector(".menuButton");
+// let menu      = document.querySelector("nav");
+// let menuClose = document.querySelector("nav .close");
+// let menuOpen  = document.querySelector(".menuButton");
 
-function toggleClass(object, className) {
-	if(object.classList.contains(className)) {
-		object.classList.remove(className);
-	} else {
-		object.classList.add(className);
+// function toggleClass(object, className) {
+// 	if(object.classList.contains(className)) {
+// 		object.classList.remove(className);
+// 	} else {
+// 		object.classList.add(className);
+// 	}
+// 	return;
+// }
+
+// menuClose.onclick = () => {
+// 	toggleClass(menu, "hidden");
+// 	toggleClass(menuOpen, "hidden");
+// };
+
+// menuOpen.onclick = () => {
+// 	toggleClass(menu, "hidden");
+// 	toggleClass(menuOpen, "hidden");
+// };
+
+// document.body.onload = () => {
+// 	document.body.classList.remove("preload");
+// };
+
+const show_menu = (toggleId, navId, bodyId)=>{
+	const toggle = document.getElementById(toggleId),
+	navbar = document.getElementById(navId),
+	bodypadding = document.getElementById(bodyId)
+
+	if(toggle && navbar){
+		toggle.addEventListener('click',()=>{
+			navbar.classList.toggle('expander')
+
+			bodypadding.classList.toggle('body-pd')
+		})
 	}
-	return;
 }
 
-menuClose.onclick = () => {
-	toggleClass(menu, "hidden");
-	toggleClass(menuOpen, "hidden");
-};
+show_menu('naw-toggle','nawigacja','body-pd');
 
-menuOpen.onclick = () => {
-	toggleClass(menu, "hidden");
-	toggleClass(menuOpen, "hidden");
-};
+const linkColor = document.querySelectorAll('.naw_link')
+function colorLink(){
+  linkColor.forEach(l=> l.classList.remove('active'))
+  this.classList.add('active')
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
-document.body.onload = () => {
-	document.body.classList.remove("preload");
-};
+const linkC = document.getElementsByClassName('c_link')
+var i;
+
+for(i=0;i<linkC.length;i++){
+  linkC[i].addEventListener('click', function(){
+    const cMenu = this.nextElementSibling
+    cMenu.classList.toggle('showC')
+
+	const rotate = cMenu.previousElementSibling
+    rotate.classList.toggle('rotate')
+  })
+}
